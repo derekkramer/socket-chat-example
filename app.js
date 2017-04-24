e5827895dbfbe39a13c10456838546bd66bcd94e
Derek Kramer <derek.kramer88@gmail.com>
2017-04-24 15:29:14 -0600

    💬 Added chat capability

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-    console.log('A user connected!')
+    io.on('connection', function(socket) {
+        socket.on('chat message', function(msg) {
+            io.emit('chat message', msg)
+        })
+    })
 })
 
 http.listen(3000, () => {
